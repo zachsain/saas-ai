80e3020fa2deb52ab92ea44737cf2665a8a4ad08
Zachary Sain <zachsain@gmail.com>
2023-11-17 17:00:32 -0600

    dashboard work

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -4,8 +4,19 @@ import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import Sidebar from "@/components/sidebar";
+import { useEffect, useState } from "react";
 
 export default function MobileSidebar() {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted){
+    return null
+  }
+  
   return (
     <div>
       <Sheet>
