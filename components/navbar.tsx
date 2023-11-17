70b4b029aa9196a2ff961423374b309e2bb880d2
Zachary Sain <zachsain@gmail.com>
2023-11-17 16:27:59 -0600

    collapse sidebar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
-import { Menu } from "lucide-react";
+import MobileSidebar from "@/components/mobile-sidebar";
 
 
 export default function NavBar() {
   return (
     <div className="flex items-center p-4">
-        <Button variant="ghost" size="icon" className="md:hidden">
-            <Menu />
-        </Button>
+        <MobileSidebar/>
         <div className="flex w-full justify-end">
             <UserButton afterSignOutUrl="/" />
         </div>
