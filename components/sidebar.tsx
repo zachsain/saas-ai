d164072d9ead7babd9fb96eafdedc357f5108960
Zachary Sain <zachsain@gmail.com>
2023-11-17 15:54:23 -0600

    sidebar font etc

---
+++ components/sidebar.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { Montserrat } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+
+const montserrat = Montserrat({weight: "600", subsets: ["latin"]});
+
+export default function Sidebar() {
+  return (
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+      <div className="px-3 py-2 flex-1">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-14"> 
+        <div className="relative w-8 h-8 mr-4">
+            <Image
+            fill 
+            alt="logo"
+            src="/logo.png"
+            />
+        </div>
+        <h1 className={cn ("text-2xl font-bold", montserrat.className)}>Genius</h1>
+        </Link>
+      </div>
+    </div>
+  );
+}
